0b7e5e5efa7191a7c5c1e31d6b09bd2303855c6a
Brendon Ferreira <brendonferreiradm@gmail.com>
2017-04-26 11:40:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const periodValid = (datePeriod) => {
     return ( 
         datePeriod && (
             datePeriod.undefined || (
-                (today.getHours() / 60) + today.getMinutes() >= (dateIn.hour / 60) + dateIn.minute )  &&  
-                (today.getHours() / 60) + today.getMinutes() <= (dateOut.hour / 60) + dateOut.minute )  &&   
+                (today.getHours() / 60) + today.getMinutes() >= (dateIn.hour / 60) + dateIn.minute &&  
+                (today.getHours() / 60) + today.getMinutes() <= (dateOut.hour / 60) + dateOut.minute &&   
                 todayDate.getTime() >= jsonToDate(dateIn).getTime() &&
                 todayDate.getTime() <= jsonToDate(dateOut).getTime() &&
                 datePeriod.week[ weekNames[today.getDay()] ] != false 
